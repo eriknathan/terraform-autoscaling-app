69944115742c07a0b4d504ff623c143cff4f8bfb
eriknathan <eriknathan.contato@gmail.com>
2024-09-11 15:16:37 -0300

    feat: provider

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+  backend "s3" {
+    bucket = "s3-erik-tf"
+    key    = "autoscaling-app/terraform.tfstate"
+    region = "us-east-1"
+  }  
+}
+
+provider "aws" {
+  region = var.region
+}
