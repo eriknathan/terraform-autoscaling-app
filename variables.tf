fdba059e9df5a83e2375ebea1688e800b099e552
eriknathan <eriknathan.contato@gmail.com>
2024-09-11 15:17:11 -0300

    feat: variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "Project Name"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR BLOCK to VPC"
+}
+
+variable "region" {
+  type        = string
+  description = "Region AWS"
+}
